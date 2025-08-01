771c730bd6c8365e819e226ea9466c2c0dca40a4
Whiz225 <nwosuemmanuel1994@gmail.com>
2025-08-01 15:13:39 +0100

    vite.config update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   css: {
+//     postcss: "./postcss.config.js",
+//   },
+//   publicDir: "public",
+//   root: ".",
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    postcss: "./postcss.config.js",
-  },
+  root: "./",
   publicDir: "public",
+  base: "/assessment/",
+  server: {
+    open: true,
+  },
+  css: {
+    postcss: "./postcss.config.cjs",
+  },
 });

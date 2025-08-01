08c437215407e989011c2155111ff725d38a8227
Whiz225 <nwosuemmanuel1994@gmail.com>
2025-08-01 16:09:12 +0100

    css update

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,20 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   css: {
-//     postcss: "./postcss.config.js",
-//   },
-//   publicDir: "public",
-//   root: ".",
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: "./",
-  publicDir: "public",
   base: "/assessment/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
+    },
+  },
   server: {
     open: true,
   },
